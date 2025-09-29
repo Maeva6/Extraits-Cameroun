a601b875ddd4b03f6f470fc817a3f8eb94ce0239
depaye99 <mongolaurent92@gmail.com>
2025-09-29 22:44:31 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,25 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import tailwindcss from '@tailwindcss/vite'
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
-        "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-   
-    ],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/js/**/*.jsx',
+    './resources/**/*.js',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
     },
+  },
 
-    plugins: [
-        tailwindcss,
-        forms],
+  plugins: [
+    forms,
+  ],
 };
