ab923116264f00af14f61881cd15b85741b60210
depaye99 <mongolaurent92@gmail.com>
2025-09-30 08:21:10 +0100

    vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css','resources/js/app.jsx'],
+            input: ['resources/css/app.css','resources/js/Pages/Dashboard.jsx'],
             refresh: true,
         }),
         react(),
