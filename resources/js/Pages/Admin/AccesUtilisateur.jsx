d9a0716299f334b915501094f676fd1f7233ef88
depaye99 <mongolaurent92@gmail.com>
2025-09-30 12:46:02 +0100

    .2

--- resources/js/Pages/Admin/AccesUtilisateur.jsx
+++ resources/js/Pages/Admin/AccesUtilisateur.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AccesUtilisateur from '../../Components/AccesUtilisateur';
+import AccesUtilisateur from '../../Pages/Admin/accesUtilisateurAdmin';
 
 export default function AccesUtilisateurPage({ user }) {
     return <AccesUtilisateur user={user} />;
