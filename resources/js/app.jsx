02e04cb5f53e43395d3b2af6f929c360b70e66e5
depaye99 <mongolaurent92@gmail.com>
2025-10-02 14:33:51 +0100

    app.css updated

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -32,36 +32,3 @@ createInertiaApp({
     },
 });
 
-// import '../css/app.css';
-// import './bootstrap';
-
-// import { createInertiaApp } from '@inertiajs/react';
-// import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-// import { createRoot } from 'react-dom/client';
-// import { StrictMode } from 'react';
-// import AppWrapper from './components/App'; // ⚠️ AppWrapper doit encapsuler ton layout global (cart, favoris, etc.)
-
-// const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
-// createInertiaApp({
-//   title: (title) => `${title} - ${appName}`,
-//   resolve: (name) =>
-//     resolvePageComponent(
-//       `./Pages/${name}.jsx`,
-//       import.meta.glob('./Pages/**/*.jsx')
-//     ),
-//   setup({ el, App, props }) {
-//     const root = createRoot(el);
-
-//     root.render(
-//       <StrictMode>
-//         <AppWrapper>
-//           <App {...props} />
-//         </AppWrapper>
-//       </StrictMode>
-//     );
-//   },
-//   progress: {
-//     color: '#4B5563',
-//   },
-// });
